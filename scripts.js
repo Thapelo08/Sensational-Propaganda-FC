caf5024cd92d0163c0aaf7fbe2240692f7dba5b7
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-06-24 14:50:12 +0200

    "Added event listeners to nav toggle and nav links"

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelector('.nav__link');
+
+navToggle.addEventListener('click', function() {
+     document.body.classList.toggle('nav-open')
+});
+ 
+navLinks.foreach(link => {
+    link.addEventListener('click', function() {
+        document.body.classList.remove('nav-open');
+    })
+})
